4263b92f674229f982754b616df3ad2051abf172
Elisha Peterson <triathematician@gmail.com>
2026-03-07 11:47:05 -0500

    fix module issue with reflective access to get children method

--- src/main/java/tornadofx/FX.kt
+++ src/main/java/tornadofx/FX.kt
@@ -679,13 +679,16 @@ fun EventTarget.getChildList(): MutableList<Node>? = when (this) {
 }
 
 @Suppress("UNCHECKED_CAST", "PLATFORM_CLASS_MAPPED_TO_KOTLIN")
-private fun Parent.getChildrenReflectively(): MutableList<Node>? {
-    val getter = this.javaClass.findMethodByName("getChildren")
-    if (getter != null && java.util.List::class.java.isAssignableFrom(getter.returnType)) {
-        getter.isAccessible = true
-        return getter.invoke(this) as MutableList<Node>
+private fun Parent.getChildrenReflectively(): MutableList<Node>? = when (this) {
+    is Pane -> children as MutableList<Node>
+    is Group -> children as MutableList<Node>
+    else -> {
+        val getter = this.javaClass.findMethodByName("getChildren")
+        if (getter != null && java.util.List::class.java.isAssignableFrom(getter.returnType)) {
+            getter.isAccessible = true
+            getter.invoke(this) as MutableList<Node>
+        } else null
     }
-    return null
 }
 
 var Window.aboutToBeShown: Boolean
